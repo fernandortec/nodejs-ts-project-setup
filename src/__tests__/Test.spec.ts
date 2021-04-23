26d7908a38200381746b39bec075b87deedea4cf
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2021-04-23 18:05:47 -0300

    Add Prettier Eslint and Jest config

---
+++ src/__tests__/Test.spec.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+
+test('it should be ok', () => {
+  expect('Ok').toBe('Ok');
+});
--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,7 @@ import { router } from './router';
 const app = express();
 
 app.use(express.json());
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello world' });
-});
+app.get('/', (request, response) => response.json({ message: 'Hello world' }));
 app.use(router);
 
 export { app };
